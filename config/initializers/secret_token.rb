9339f497c50b18f86022d3838a4d673a5d8e84ee
Russell Schmidt <russell.schmidt@me.com>
2013-04-03 12:02:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resumesite::Application.config.secret_token = 'a54b5b440d9ff3438f14f8c0eb7ff14ce9938d70426b2c9b71e4385bebf02f94e5cdb45952c0ff2ba061d171c9c600ee7680064da420113ee252ffa4ec2c2b35'
