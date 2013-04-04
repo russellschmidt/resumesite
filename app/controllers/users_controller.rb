8d63db5de3df303a92bc067cdb30aadc4c7169aa
Russell Schmidt <russell.schmidt@me.com>
2013-04-03 18:18:08 -0700

    user section done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,39 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
-  def new
-    @user = User.new
-  end
-
   def show
   	@user = User.find(params[:id])
+  end
+
+
+  def new
+    @user = User.new
   end
 
   def create
   	@user = User.new(params[:user])
 
     if @user.save
+      #flash[:success] = "Another noob is made"
       redirect_to @user
     else
       render action: :new
     end
   end
 
+
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      #flash[:success] = "Successful update is successful"
+      redirect_to @user
+    else
+      render action: :edit
+    end
+  end
+
 end
